fb5104d1847fc9703de6adce9fe172af88af97d5
Kevin Pulley <kpulley@imaginecommunications.com>
2021-10-18 18:08:28 -0400

    working again

--- plan/dd.py
+++ plan/dd.py
@@ -4,14 +4,13 @@ import sys
 def metaclean(path):
     rcode = 0
     ofstring = 'of=' + path
-    sdcmd = ['echo','smart','|','sudo','-S','pwd']
-    sdresult = run(sdcmd, stdout = PIPE, stderr = PIPE, universal_newlines = True)
-    szcmd = ['sudo', 'blockdev', '--getsz', path]
+    
+    szcmd = ['echo','smart','|','sudo','-S','sudo', 'blockdev', '--getsz', path]
     # find the end of the drive
     szresult = run(szcmd, stdout = PIPE, stderr = PIPE, universal_newlines = True)
     rcode = rcode + szresult.returncode
     # zero first 16 miB
-    ddcmd = ['sudo','dd','if=/dev/zero',ofstring,'bs=4096','count=4096']
+    ddcmd = ['echo','smart','|','sudo','-S','sudo','dd','if=/dev/zero',ofstring,'bs=4096','count=4096']
     ddresult = run(ddcmd, stdout = PIPE, stderr = PIPE, universal_newlines = True)
     rcode = rcode + ddresult.returncode
 
@@ -19,7 +18,7 @@ def metaclean(path):
 
     seek = int(szresult.stdout)-1024
     seekstr = 'seek=' + str(seek) 
-    ddcmd = ['sudo', 'dd', 'if=/dev/zero', ofstring,'bs=512', seekstr, 'count=4096']
+    ddcmd = ['echo','smart','|','sudo','-S','sudo', 'dd', 'if=/dev/zero', ofstring,'bs=512', seekstr, 'count=4096']
 
     ddresult = run(ddcmd, stdout = PIPE, stderr = PIPE, universal_newlines = True)
     rcode = rcode + ddresult.returncode
