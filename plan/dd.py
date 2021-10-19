6d8912f4bf3275a7e5b7aced75d378a5b13dc628
Kevin Pulley <kpulley@imaginecommunications.com>
2021-10-19 09:07:27 -0400

    fixed dd to clean metadata

--- plan/dd.py
+++ plan/dd.py
@@ -10,16 +10,15 @@ def metaclean(path):
     rcode.append(szresult[0].decode('utf-8'))
     # zero first 16 miB
     ddcmd = ['sudo','dd','if=/dev/zero',ofstring,'bs=4096','count=4096']
-    ddresult = subprocess.Popen(ddcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate(input=b'smart\n')
-    rcode.append(ddresult[0].decode('utf-8'))
+    ddresult = subprocess.Popen(ddcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE))#.communicate(input=b'smart\n')
+    rcode.append(ddresult[1].decode('utf-8'))
 
     # zero end of drive
 
-    seek = int(szresult[0].decode('utf-8'))-1024
+    seek = int(szresult[1].decode('utf-8'))-1024
     seekstr = 'seek=' + str(seek) 
-    print(seekstr)
-    ddcmd = ['sudo', 'dd', 'if=/dev/zero', ofstring,'bs=512', seekstr, 'count=4096']
+    ddcmd = ['sudo', 'dd', 'if=/dev/zero', ofstring,'bs=512', seekstr, 'count=1024']
 
-    ddresult = subprocess.Popen(ddcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate(input=b'smart\n')
-    rcode.append(ddresult[0].decode('utf-8'))
+    ddresult = subprocess.Popen(ddcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE))#.communicate(input=b'smart\n')
+    rcode.append(ddresult[1].decode('utf-8'))
     return(rcode)
