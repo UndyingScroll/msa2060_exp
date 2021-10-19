bd4a96ed50b3befae909bb5bc9b0ae4d900290fc
Kevin Pulley <kpulley@imaginecommunications.com>
2021-10-19 09:49:36 -0400

    reduced dd results

--- plan/dd.py
+++ plan/dd.py
@@ -5,13 +5,15 @@ def metaclean(path):
     rcode = []
     ofstring = 'of=' + path
     # find the end of the drive
+    rcode.append(ofstring)
     szcmd = ['sudo', 'blockdev', '--getsz', path]
     szresult = subprocess.Popen(szcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate(input=b'smart\n')
     rcode.append(szresult[0].decode('utf-8'))
     # zero first 16 miB
     ddcmd = ['sudo','dd','if=/dev/zero',ofstring,'bs=4096','count=4096']
     ddresult = subprocess.Popen(ddcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate(input=b'smart\n')
-    rcode.append(ddresult[1].decode('utf-8'))
+    result = ddresult[1].decode('utf-8')
+    rcode.append(result[:25])
 
     # zero end of drive
 
@@ -20,5 +22,6 @@ def metaclean(path):
     ddcmd = ['sudo', 'dd', 'if=/dev/zero', ofstring,'bs=512', seekstr, 'count=1024']
 
     ddresult = subprocess.Popen(ddcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate(input=b'smart\n')
-    rcode.append(ddresult[1].decode('utf-8'))
+    result = ddresult[1].decode('utf-8')
+    rcode.append(result[:25])
     return(rcode)
